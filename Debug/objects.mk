9d84b42739b8c07b1658d18a7f463dc43ddafffa
Vinh hoa Nguyen <vinh-hoa.nguyen@thuasne.fr>
2021-04-22 17:55:03 +0200

    Test security

---
+++ Debug/objects.mk
@@ -0,0 +1,9 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -l"C:/Users/vnguyen/STM32CubeIDE/workspace_1.6.0/Test_Security/Middleware/ST/STM32_Crypto/Lib/STM32CryptographicV3.0.0_CM4_GCC.a" -l"C:/Users/vnguyen/STM32CubeIDE/workspace_1.6.0/Test_Security/Middleware/ST/STM32_Crypto_AccHw/Lib/STM32AccHwCryptoV3.1.0_L4_GCC.a"
+
